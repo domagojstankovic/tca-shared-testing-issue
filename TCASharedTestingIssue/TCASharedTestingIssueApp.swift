b894c20505aaa39274362b494c24862ce0108e30
Domagoj Stankovic <domagoj.stankovic@gmail.com>
2024-08-12 15:56:45 +0200

    Initial Commit

---
+++ TCASharedTestingIssue/TCASharedTestingIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCASharedTestingIssueApp.swift
+//  TCASharedTestingIssue
+//
+//  Created by Domagoj Stankovic on 12.08.2024..
+//
+
+import SwiftUI
+
+@main
+struct TCASharedTestingIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
